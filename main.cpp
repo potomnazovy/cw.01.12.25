16728f2d568d24349eb95a8f8378922c62837721
potomnazovy <skeleton39876@mail.ru>
2025-12-01 16:59:53 +0300

    IDraw interface

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
+#include <cstddef>
+namespace top
+{
+  struct p_t
+  {
+    int x, y;
+  };
+  struct IDraw
+  {
+    virtual p_t begin() = 0;
+    virtual p_t next(p_t) = 0;
+  };
+  bool operator==(p_t a, p_t b)
+  {
+    return a.x == b.x && a.y == b.y;
+  }
+  bool operator!=(p_t a, p_t b)
+  {
+    return a.x != b.x || a.y != b.y;
+  }
+}
 int main()
 {
   return 0;
