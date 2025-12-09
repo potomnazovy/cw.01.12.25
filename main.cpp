d780d361047166a71c393c21153ad7bf96ab87a5
potomnazovy <skeleton39876@mail.ru>
2025-12-09 21:06:16 +0300

    complete hw

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstddef>
+#include <algorithm>
 
 namespace top 
 {
@@ -64,7 +65,25 @@ namespace top
     p_t start;
     int length;
   };
+
+  struct DiagonalLine : IDraw
+  {
+    DiagonalLine(int x, int y, int len);
+    p_t begin() const override;
+    p_t next (p_t p) const override;
+    p_t start;
+    int length;
+  };
   
+  struct Rectangle : IDraw
+  {
+    Rectangle(int x, int y, int a, int b);
+    p_t begin() const override;
+    p_t next (p_t p) const override;
+    p_t start;
+    int a_, b_;
+  };
+
   struct Square: IDraw 
   {
     Square(int x, int y, int l);
@@ -90,15 +109,15 @@ size_t top::cols(Frame_t fr)
 
 int main()
 {
-  top::IDraw* f[5] = {};
+  top::IDraw* f[7] = {};
   top::p_t* p = nullptr;
   size_t s = 0;
   char* cnv = nullptr;
   int statusCode = 0;
   try 
   {
-    top::make_f(f, 5);
-    for (size_t i = 0; i < 5; ++i) 
+    top::make_f(f, 7);
+    for (size_t i = 0; i < 7; ++i) 
     {
       top::getPoints(f[i], &p, s);
     }
@@ -117,6 +136,8 @@ int main()
   delete f[2];
   delete f[3];
   delete f[4];
+  delete f[5];
+  delete f[6];
   delete[] p;
   delete[] cnv;
 
@@ -135,6 +156,8 @@ void top::make_f(IDraw** f, size_t k)
   f[2] = new Square(7, 7, 3);
   f[3] = new VLine(4, 5, 3);
   f[4] = new HLine(2, 7, 4);
+  f[5] = new DiagonalLine(-3, 1, 4);
+  f[6] = new Rectangle(-10, -4, 4, 7);
 }
 
 void top::extend(p_t** ps, size_t s, p_t p)
@@ -281,8 +304,34 @@ top::Square::Square(int x, int y, int l):
 {
   if (len <= 0) 
   {
-    throw std::invalid_argument("lenght can not be <= 0");
+    throw std::invalid_argument("length can not be <= 0");
   }
+}
+
+top::DiagonalLine::DiagonalLine(int x, int y, int len) : IDraw(), start{x, y}, length(len)
+{
+  if (len <= 0)
+  {
+    throw std::logic_error("user invalid");
+  }
+}
+
+top::p_t top::DiagonalLine::begin() const
+{
+  return start;
+}
+
+top::p_t top::DiagonalLine::next(p_t p) const
+{
+  if (p.x - start.x == p.y - start.y)
+  {
+    if (p.x == start.x + length - 1 && p.y == start.y + length - 1)
+    {
+      return start;
+    }
+    return {p.x + 1, p.y + 1};
+  }
+  return start;
 }
 
 top::Square::Square(p_t p, int l):
@@ -309,6 +358,40 @@ top::p_t top::Square::next(p_t p) const
     return {p.x, p.y - 1};
   } 
   else if (p.y == start.y && p.x > start.x) 
+  {
+    return {p.x - 1, p.y};
+  }
+  return start;
+}
+
+top::Rectangle::Rectangle(int x, int y, int a, int b) : IDraw(), start{x, y}, a_(a), b_(b)
+{
+  if (a <= 0 || b <= 0)
+  {
+    throw std::logic_error("user invalid");
+  }
+}
+
+top::p_t top::Rectangle::begin() const
+{
+  return start;
+}
+
+top::p_t top::Rectangle::next(p_t p) const
+{
+  if (p.x == start.x && p.y < start.y + b_ - 1)
+  {
+    return {p.x, p.y + 1};
+  }
+  else if (p.y == start.y + b_ - 1 && p.x < start.x + a_ - 1)
+  {
+    return {p.x + 1, p.y};
+  }
+  else if (p.x == start.x + a_ - 1 && p.y > start.y)
+  {
+    return {p.x, p.y - 1};
+  }
+  else if (p.y == start.y && p.x > start.x)
   {
     return {p.x - 1, p.y};
   }
